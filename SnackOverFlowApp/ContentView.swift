6d40645929b9e8f94c52141ac27d468b33aa2f78
Yunus Emre Şimşek <yemresimseek@gmail.com>
2024-07-19 19:12:17 +0300

    Initial Commit

---
+++ SnackOverFlowApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnackOverFlowApp
+//
+//  Created by Emre Simsek on 19.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnackOverFlowApp/SnackOverFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackOverFlowAppApp.swift
+//  SnackOverFlowApp
+//
+//  Created by Emre Simsek on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnackOverFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
